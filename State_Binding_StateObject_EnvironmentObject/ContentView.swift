38694ff58ecbac6a72a8105badcec9bf479215aa
hooneun <kimhoon0316@gmail.com>
2023-12-29 21:21:37 +0900

    Initial Commit

---
+++ State_Binding_StateObject_EnvironmentObject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  State_Binding_StateObject_EnvironmentObject
+//
+//  Created by Hoon on 12/29/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ State_Binding_StateObject_EnvironmentObject/State_Binding_StateObject_EnvironmentObjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  State_Binding_StateObject_EnvironmentObjectApp.swift
+//  State_Binding_StateObject_EnvironmentObject
+//
+//  Created by Hoon on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct State_Binding_StateObject_EnvironmentObjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
